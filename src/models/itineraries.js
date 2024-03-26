4b741dd1ea59bbf23ad9fbc94660c4916dc23cdb
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-03-26 19:29:18 -0400

    Added vibe to itinerary schema with enum option to limit to 3 different vibes

--- src/models/itineraries.js
+++ src/models/itineraries.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose')
 
+const activitySchema = new mongoose.Schema({
+    description: String,
+    time: String,
+    location: String,
+    cost: {
+        type: Number,
+        set: v => Math.round(v * 100) / 100, 
+        validate: {
+            validator: function(v) {
+                return (v * 100) % 1 === 0
+            },
+            message: '{VALUE} is not a valid currency amount. Please use a value with up to two decimal places.'
+        }
+    },
+    note: String,
+})
+// The logic above will round any entered numbers to the hundredth decimal before running a validation to make sure that it's a legit number.
+// It works by multiplying the submitted number by 100, then rounds to the nearest whole number, then divides by 100 again to go back to the original amount, which is now rounded to the hundredth decimal.
+
+
 const daySchema = new mongoose.Schema({
     date: Date,
-    activities: [{
-        description: String,
-        time: String,
-        location: String,
-    }]
+    activities: [activitySchema]
 })
 
 const itinerarySchema = new mongoose.Schema({
@@ -14,6 +30,10 @@ const itinerarySchema = new mongoose.Schema({
     startDate: Date,
     endDate: Date,
     days: [daySchema],
+    vibe: {
+        type: String,
+        enum: ['adventure', 'romantic', 'family-friendly'],
+    }
 })
 
 // Pre-save hook
