fe55d25d907a6f7403830e491cc44803f4411509
vmlayton87 <vmlayton87@gmail.com>
2024-03-28 14:19:37 -0400

    .get by vibe

--- src/server/controllers/itinerary_controller.js
+++ src/server/controllers/itinerary_controller.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import Itinerary from "../models/itineraries.js"
 
-
+// get all itineraries
 router.get ("/", (req, res)=>{
     // res.send("Hello from the itinerary get call.")
     Itinerary.find()
@@ -14,4 +14,16 @@ router.get ("/", (req, res)=>{
     .catch(err=>{console.log(err)})
   })
 
+  // get itineraries by vibe key
+  router.get("/:vibe", (req, res)=>{
+    Itinerary.find({vibe: { $regex: req.params.vibe, $options: 'i'}}).exec()
+    // for this to work best, need to make the get request based on an input dropdown or selection that way it doesn't return an empty array if someone misspells something.
+    // name: { $regex: searchString, $options: 'i' }  this will search for a regular expression that matches req.params.vibe
+    // MyModel.find({ name: /john/i }, 'name friends').exec(); from the docs
+    .then(foundItenerary => {
+      console.log(foundItenerary)
+      res.send(foundItenerary)
+    })
+    .catch(err=>{console.log(err)})
+  })
   export default router
