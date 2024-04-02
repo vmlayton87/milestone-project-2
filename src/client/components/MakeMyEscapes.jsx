db8681b5de5a41c9c02996dfadeb1bb11b66dad0
Andrew Thomas <athomas94@gmail.com>
2024-04-01 20:00:16 -0400

    added basis of form for new escapes

--- src/client/components/MakeMyEscapes.jsx
+++ src/client/components/MakeMyEscapes.jsx
@@ -1,11 +1,76 @@
+import React, { useState } from 'react';
+const Default = require('../default')
 
+function newEscape() {
+  const [formData, setFormData] = useState({
+    destination: '',
+    startDate: '',
+    endDate: '',
+    vibe: ''
+  })
 
-const MakeMyEscapes = () => {
-    return (
-      <div style={{height:'80vh'}}>
-      <h1>This is the MakeMyEscapes page</h1>
-    </div>
-    );
+  const handleInput = (event) => {
+    const { destination, value } = event.target;
+    setFormData({
+      ...formData,
+      [destination]: value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // submit to server here
+    console.log(formData);
+  };
+
+  return (
+    <Default>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Destination
+          <input
+            type="text"
+            name="destination"
+            value={formData.destination}
+            onChange={handleInput}
+          />
+        </label>
+        <br />
+        <label>
+          Start Date
+          <input
+            type="date"
+            name="startDate"
+            value={formData.startDate}
+            onChange={handleInput}
+          />
+        </label>
+        <br />
+        <label>
+          End Date
+          <input
+            type="date"
+            name="endDate"
+            value={formData.endDate}
+            onChange={handleInput}
+          />
+        </label>
+        <br />
+        <label>
+          Vibe
+          <input
+            type="text"
+            name="vibe"
+            value={formData.vibe}
+            onChange={handleInput}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+  </Default>
+  );
+
 }
 
-export default MakeMyEscapes;
+export default newEscape;
