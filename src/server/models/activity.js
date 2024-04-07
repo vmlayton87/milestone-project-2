95a5f277d88fdcec2b85b03c1d56cae850b7e1d5
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-04-07 08:56:46 -0400

    Updated dist folder for deployment

--- src/server/models/activity.js
+++ src/server/models/activity.js
@@ -16,8 +16,7 @@ const activitySchema = new mongoose.Schema({
     },
     note: String,
 })
-// The logic above will round any entered numbers to the hundredth decimal before running a validation to make sure that it's a legit number.
-// It works by multiplying the submitted number by 100, then rounds to the nearest whole number, then divides by 100 again to go back to the original amount, which is now rounded to the hundredth decimal.
+
 
 const Activity = mongoose.model('Activity', activitySchema)
 
